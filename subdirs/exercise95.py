d32b92f1a38b2c39c86d1562b40ffd08ef8010c2
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-22 11:50:11 +0100

    bug fix

--- subdirs/exercise95.py
+++ subdirs/exercise95.py
@@ -2,6 +2,6 @@ comma_sep_input = input("Enter comma separated values: ")
 
 comma_sep_input = comma_sep_input.split(',')
 
-with open(f"comma_input_{comma_sep_input[0]}.txt", "w") as f:
+with open(f"comma_input.txt", "a") as f:
     for i in comma_sep_input:
         f.write(f"{i}\n")
