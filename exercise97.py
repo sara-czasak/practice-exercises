f1dad9b3bc7382b72b7733b9e540faa82a32ecb4
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-03-22 12:27:35 +0100

    impruving UX

--- exercise97.py
+++ exercise97.py
@@ -3,14 +3,16 @@ data = []
 
 while not close:
     input_value = input("Enter value: ")
-    if input_value != 'CLOSE' and input_value != 'SAVE':
+    if input_value.upper() != 'CLOSE' and input_value.upper() != 'SAVE':
         data.append(input_value)
-    elif input_value == 'CLOSE':
+    elif input_value.upper() == 'CLOSE':
+        print("*** Closing and saving... ***")
         close = True
     else:
         with open("notes1.txt", "a+") as file:
             for i in data:
                 file.write(f"{i}\n")
+        print("*** Notes saved successfully! ***")
 
 with open("notes1.txt", "a+") as file:
     for i in data:
